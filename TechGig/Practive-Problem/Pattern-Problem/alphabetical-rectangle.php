9bc60ef1ced5953291e14865f7489f2aa745b621
anshul91 <anshulpareek@yahoo.com>
2020-05-05 15:56:11 +0530

    Create alphabetical-rectangle.php

---
+++ TechGig/Practive-Problem/Pattern-Problem/alphabetical-rectangle.php
@@ -0,0 +1,19 @@
+<?php
+//https://www.techgig.com/practice/question/alphabetical-rectangle/TnRMeVJxS3VqQzFWNjFlbUNzMjkrNW04Wjl2U0k5VENWc0hRK0FaY1Fadz0=/1
+  /* Read input from STDIN. Print your output to STDOUT*/
+  $fp = fopen("php://stdin", "r");
+  //Write code here
+  $n = fgets($fp);
+  for($i=1;$i<=$n;$i++)
+	{
+		for($j=1;$j<=5;$j++)
+		{
+			if($j<5)
+				echo chr($i+64)." ";
+			else
+				echo char($i+64);
+		}
+		if($i<$n)
+			echo "\n";
+	}
+?>
