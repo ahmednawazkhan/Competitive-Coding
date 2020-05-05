33ca560b8b4f66fbbb7dbe51b3788ecdf1d998ba
anshul91 <anshulpareek@yahoo.com>
2020-05-05 13:52:25 +0530

    Update patternProblem39.php

--- TechGig/Practive-Problem/Pattern-Problem/patternProblem39.php
+++ TechGig/Practive-Problem/Pattern-Problem/patternProblem39.php
@@ -1,1 +1,28 @@
-https://www.techgig.com/practice/question/pattern-problem-39/WFhLRHVWVzBFR0FyYUFXZGJrZWNPVlJNTlloY1VVc2tzUXlZemMyajFSaz0=/1
+<?php
+//url: https://www.techgig.com/practice/question/pattern-problem-39/aU5kSU5hMHZCMlVML0hwNy9VQWtkWTdHZU5MdWtQYXp2UVBKOEFaVVArTT0=/1
+/*
+*Solved by : Anshul pareek
+*/
+  /* Read input from STDIN. Print your output to STDOUT*/
+  $fp = fopen("php://stdin", "r");
+  //Write code here
+  $n = fgets($fp);
+
+	for($i=1;$i<=$n;$i++)
+	{
+		for($j=1;$j<=$n-$i;$j++)
+			echo "  ";
+		$val = $i+($i-1);
+    $loop = $val+$val-1;
+    $cnt = 1;
+    for($k=1; $k<=$loop; $k++){
+      if($k%2 != 0){
+          echo $cnt++;
+      }else{
+        echo " ";
+      }
+    }
+    
+    echo $i<$n?"\n":'';
+	}
+?>
